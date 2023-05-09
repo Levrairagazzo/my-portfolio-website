8dfa253ef15b8b59bc6ecfb291b657edf31e42e5
Sydney Goeury <goeurysydney@gmail.com>
2023-05-09 14:49:16 +0200

    Getting Familiar with useContext Api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
+import { React, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
+export const UserContext = React.createContext();
 
 function App() {
+  const [french, setFrench] = useState(false);
+
   return (
     <>
-        <Routes>
+        
+          <UserContext.Provider value = {{french, setFrench}}>
+          <Routes>
           <Route path="/" element={<Home/>}/>          
           <Route path="/about" element={<About/>}/>
           <Route path="/projects" element={<Projects/>}/>
           <Route path="/resume" element={<Resume/>}/>
-        </Routes>
+          </Routes>
+          </UserContext.Provider>
+       
     </>
   
      
