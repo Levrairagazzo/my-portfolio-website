33e0ffcd520c4e3100ce83c1fab90ccc1447aa42
Sydney Goeury <goeurysydney@gmail.com>
2023-05-23 12:04:45 +0200

    Relecture version française

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,22 +1,29 @@
 import {
     AiFillGithub,
   } from "react-icons/ai";
-  import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { UserContext } from "../App";
+import { useContext } from "react";
 
 const Project = (props) => {
+
+  const { french } = useContext(UserContext);
+  
     return (
        <>
          
          <h1 className=" font-extrabold text-2xl py-3">
-            {props.project_info.title}
+            {!french && props.project_info.title}
+            {french && props.project_info.french_title}
         </h1>
         <img src={props.project_info.gif}alt="screenshot of project" className=" mx-auto hover:scale-110 transition duration-300 ease-in-out "></img>
-        <p className="text-justify p-4">{props.project_info.description}</p>
-        <p className="text-justify p-4">Tech stack: {props.project_info.techstack}</p>
+        <p className="text-justify p-4">{!french && props.project_info.description}{french && props.project_info.french_description}</p>
+        <p className="text-justify p-4">{french && "Stack technique"}{!french && "Tech stach"}: {props.project_info.techstack}</p>
         <div className=" flex place-content-center  my-3  ">
         <div className="flex place-content-center cursor-pointer hover:text-teal-500 gap-3"> 
 
-        <a href={props.project_info.repo_link} rel="noreferrer" target="_blank" className="flex gap-4 hover:scale-110 transition duration-300 ease-in-out"> <AiFillGithub className=" mt-1"/>View Repository</a>
+        <a href={props.project_info.repo_link} rel="noreferrer" target="_blank" className="flex gap-4 hover:scale-110 transition duration-300 ease-in-out">
+          <AiFillGithub className=" mt-1"/>{french && "Voir le dépôt" }{!french && "View repository"}</a>
         </div>
         </div>
        
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,17 +11,21 @@ import Mern from "../assets/Mern.gif";
 
 const projects = [{
   title: "Java Checkers",
+  french_title: "Jeu de dames en Java",
   description : "This is app replicates the game of Checkers. The user can choose either play against the computer\n \
   , another player locally or through the network. The game session sessions communicate through the TCP protocol.",
   techstack:"Java, Swing library(GUI), ServerSocket library(networking)",
+  french_description: "Cette application reproduit le jeu de dames. L'utilisateur peut choisir de jouer contre l'ordinateur, contre un autre joueur en local ou à travers le réseau. Les sessions de jeu communiquent via le protocole TCP.",
   gif:Network,
   repo_link:"https://github.com/Levrairagazzo/CheckerMultiplayerJava",
 },
 {
   title: "React NBA Memory Game",
+  french_title: "NBA Memory React.js",
   description : "This web application is a Memory game made with NBA players of the 2000s. The user has to click on a player's photo every photo can only be cliked once. \n \
-  The player cards get shuffled everytime the user clicks on a new card.  ",
+  The player cards get shuffled everytime the user clicks on a new card.",
   techstack:"Javascript, HTML, CSS, React.js ",
+  french_description: "Cette application web est un jeu de memory réalisé avec des joueurs de la NBA des années 2000. L'utilisateur doit cliquer sur la photo d'un joueur et chaque photo ne peut être cliquée qu'une seule fois. Les cartes de joueurs sont mélangées à chaque fois que l'utilisateur clique sur une nouvelle carte.",
   gif:React_Memory,
   repo_link: "https://github.com/Levrairagazzo/memory-card-react",
   
@@ -29,8 +33,10 @@ const projects = [{
 
 {
   title: "JavaScript Library App",
+  french_title: "Bibliothèque intéractive",
   description : "Web application built with Javascript that allows the user to create and delete book entries into a digital library.",
   techstack:"Javascript, HTML, CSS",
+  french_description:"Bibliothèque intéractive offrant à ses utilisateurs la possibilité de supprimer des livres du catalogue et d'en ajouter de nouveau à l'aide d'un formulaire. L'app est réalisée en suivant le paradigme orienté objet en JavaScript",
   gif:Library,
   repo_link: "https://github.com/Levrairagazzo/MyLibrary",
   
@@ -38,8 +44,10 @@ const projects = [{
 ,
 {
   title: "Mern Workout Tracker",
+  french_title: "Suivi d'entrainement",
   description : "Full-stack application made to track a workout. The app support CRUD operations utilizing a RESTful API to perform HTTP requests using the JSON format.",
   techstack:"Javascript, HTML, CSS, MongoDB, Express, React.js, Node.js",
+  french_description: "Application Full-stack de suivi d'entrainement sportif. L'application prend en charge les opérations CRUD en utilisant une REST API et effectue des requêtes HTTP à l'aide du format JSON.",
   gif:Mern,
   repo_link: "https://github.com/Levrairagazzo/Workout-App",
 },
